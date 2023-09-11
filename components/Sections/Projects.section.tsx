24ca5efc095d56fac19dd6bf0fb04ca29d2f9785
thewyolar <thewyolar@gmail.com>
2023-09-11 18:53:53 +0300

    refactor: add info about projects

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -1,39 +1,31 @@
 import type { NextComponentType } from "next";
-
-import Link from "next/link";
+import ProjectCard from "./ProjectCard.section";
 
 const Projects: NextComponentType = () => {
   return (
     <div className="my-16 px-3 font-sen" id="projects">
       <p className="text-3xl font-bold text-white">Featured Projects</p>
-      <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
-        <Link
-          href="https://vault3.live"
-          passHref
-          className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
-          target="_blank"
-          rel="noopener noreferrer">
+      <div className="my-8 flex flex-col items-center justify-center gap-5">
 
-          <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
-            <p className="text-xl font-semibold">Vault3</p>
-            <p>Your safest decentralized vault</p>
-          </div>
+        <ProjectCard
+          title={"UPAS. Backend"}
+          techs={["Java", "Spring Boot", "Docker"]}
+          description={"Backend for Unified Polyclinic Automation System"}
+          link={"https://github.com/thewyolar/esap"}
+          isComingSoon={false} />
 
-        </Link>
+        <ProjectCard
+          title={"UPAS. Application"}
+          techs={["Kotlin", "Jetpack Compose"]}
+          description={"Mobile client for Unified Polyclinic Automation System"}
+          isComingSoon={true} />
 
-        <Link
-          href="https://github.com/avneesh0612/Orbits-UI"
-          passHref
-          className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
-          target="_blank"
-          rel="noopener noreferrer">
+        <ProjectCard
+          title={"PasteShare"}
+          description={"Simple clone of the pastebin.com"}
+          isComingSoon={true}
+          techs={["Java", "Spring Boot", "ReactJS(NextJS)", "TypeScript"]} />
 
-          <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
-            <p className="text-xl font-semibold">Orbits UI</p>
-            <p>React component library built with Tailwind CSS</p>
-          </div>
-
-        </Link>
       </div>
     </div>
   );
