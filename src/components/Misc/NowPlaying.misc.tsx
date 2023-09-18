024388f3a22190f80f3030cf5fabf00b3af115d4
thewyolar <thewyolar@gmail.com>
2023-09-18 16:36:40 +0300

    feat: add artist to component for display now playing misc

--- src/components/Misc/NowPlaying.misc.tsx
+++ src/components/Misc/NowPlaying.misc.tsx
@@ -12,26 +12,34 @@ const SpotifyCard: NextComponentType = () => {
   const { t } = useTranslation();
   const { data, error } = useSWR<NowPlayingSong>("/api/now-playing", fetcher);
 
-  return <>
-    <div className="font-sen mb-8 flex max-w-full flex-row items-center gap-x-2 rounded-md text-center text-lg text-gray-300">
-      <Image
-        src="/assets/spotify.svg"
-        width="40"
-        height="40"
-        alt="spotify icon"
-      />
-      {data?.isPlaying ? (
+  return (
+    <div className="font-sen mb-8 p-4 bg-gray-800 rounded-md">
+      <div className="flex items-center gap-x-4">
+        <div className="relative w-8 h-8">
+          <Image
+            src="/assets/spotify.svg"
+            alt="spotify icon"
+            layout="fill"
+          />
+        </div>
+        {data?.isPlaying ? (
           <Link href={data?.songUrl} passHref legacyBehavior>
-            <p className="cursor-pointer truncate">
-              {t('spotify.yes')}{" "}
-              <span className="text-white">{data?.title}</span>.
-            </p>
+            <div className="cursor-pointer">
+              <p className="text-white text-base truncate">
+                {t('spotify.yes')}{" "}
+                <span className="text-green-500">{data?.title}</span>
+              </p>
+              <p className="text-gray-400 text-sm">
+                {t('spotify.by')} {data?.artist}
+              </p>
+            </div>
           </Link>
-      ) : (
-          <p>{t('spotify.no')}</p>
-      )}
+        ) : (
+          <p className="text-white">{t('spotify.no')}</p>
+        )}
+      </div>
     </div>
-  </>;
+  );
 };
 
 export default SpotifyCard;
