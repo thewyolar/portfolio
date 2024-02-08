1a1b35e3dfadc3cd9721055d2a50881054d1570e
thewyolar <thewyolar@gmail.com>
2024-02-08 22:30:13 +0300

    refactor: skills section

--- src/components/Sections/Skills.section.tsx
+++ src/components/Sections/Skills.section.tsx
@@ -1,37 +1,31 @@
 import { BsArrowRightShort } from "react-icons/bs";
 import {useTranslation} from "next-i18next";
 
+type Skill = {
+  name: string;
+  description: string;
+}
+
 const Skills = () => {
   const { t } = useTranslation();
+
+  const data: Skill[] = Object.values(t('skills.data', { returnObjects: true })).map((skill: Skill) => ({
+    name: skill.name,
+    description: skill.description,
+  }));
 
   return (
     <div className="my-16 px-3 font-sen text-white" id="skills">
       <p className="text-3xl font-bold text-white">{t('skills.title')}</p>
 
       <div className="text-md my-8 flex flex-col font-medium md:text-xl custom:text-lg">
-        <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
-          <span className="text-white">{t('skills.java.name')}</span>
-          &nbsp;{t('skills.java.description')}
-        </p>
-
-        <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
-          <span className="text-white">{t('skills.spring.name')}</span>
-          &nbsp;{t('skills.spring.description')}
-        </p>
-
-        <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
-          <span className="text-white">{t('skills.postgresql.name')}</span>
-          &nbsp;{t('skills.postgresql.description')}
-        </p>
-
-        <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
-          <span className="text-white">{t('skills.docker.name')}</span>
-          &nbsp;{t('skills.docker.description')}
-        </p>
+        {data.map((skill: Skill) => (
+          <div key={skill.name} className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
+            <BsArrowRightShort size="30"/>
+            <span className="text-white">{skill.name}</span>
+            &nbsp;{skill.description}
+          </div>
+        ))}
       </div>
 
       <p className="text-lg font-medium text-slate-300">
