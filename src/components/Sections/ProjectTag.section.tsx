c6fe098d866a12765ddba2df2406bd66e550fe6e
thewyolar <thewyolar@gmail.com>
2024-02-10 00:28:45 +0300

    refactor: project tags icons

--- src/components/Sections/ProjectTag.section.tsx
+++ src/components/Sections/ProjectTag.section.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import Icon from "@mdi/react";
-import {mdiAndroid, mdiDatabase, mdiDocker, mdiLanguageJava, mdiLanguageKotlin, mdiLanguageTypescript} from "@mdi/js";
+import {SiJetpackcompose, SiKotlin, SiSpring, SiTypescript, SiAndroid} from "react-icons/si";
+import {FaDatabase, FaDocker, FaJava} from "react-icons/fa";
+import {TbBrandNextjs} from "react-icons/tb";
 
 type Props = {
   tag: string;
 };
 
-const names = {
-  "Java": mdiLanguageJava,
-  "Kotlin": mdiLanguageKotlin,
-  "PostgreSQL": mdiDatabase,
-  "Docker": mdiDocker,
-  "TypeScript": mdiLanguageTypescript,
-  "Android": mdiAndroid
+const names: Record<string, React.ReactNode> = {
+  "Java": <FaJava color="white" className="h-5" />,
+  "Kotlin": <SiKotlin color="white" className="h-5" />,
+  "PostgreSQL": <FaDatabase color="white" className="h-5" />,
+  "Docker": <FaDocker color="white" className="h-5"/>,
+  "TypeScript": <SiTypescript color="white" className="h-5" />,
+  "Android": <SiAndroid color="white" className="h-5" />,
+  "Spring Boot": <SiSpring color="white" className="h-5" />,
+  "Jetpack Compose": <SiJetpackcompose color="white" className="h-5" />,
+  "Next JS": <TbBrandNextjs color="white" className="h-5" />
 };
 
-const ProjectTag: React.FC<Props> = ({tag}) => {
+const ProjectTag: React.FC<Props> = ({ tag }) => {
   return (
     <span className="tag">
-      {names[tag] && <Icon path={names[tag]} className="h-5"/>}
+      {names[tag] && names[tag]}
       <p className="text-slate-400 text-base">{tag}</p>
     </span>
-  )
-}
+  );
+};
 
 export default ProjectTag;
