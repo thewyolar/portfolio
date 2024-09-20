5917b6c7015654464fb5ea47bb906687735d276d
Alexey Karabanov <akarabanov@wsoft.ru>
2024-09-20 19:04:07 +0300

    refactor: update view of skills cards

--- src/components/Sections/Skills.section.tsx
+++ src/components/Sections/Skills.section.tsx
@@ -1,48 +1,33 @@
-import { BsArrowRightShort } from "react-icons/bs";
 import {useTranslation} from "next-i18next";
 
-type Skill = {
-  name: string;
-  description: string;
-}
-
 const Skills = () => {
-  const { t } = useTranslation();
-  const more: string[] = Object.values(t('skills.more.data', { returnObjects: true })).map(value => String(value));
-  const data: Skill[] = Object.values(t('skills.data', { returnObjects: true })).map((skill: Skill) => ({
-    name: skill.name,
-    description: skill.description,
-  }));
+    const { t } = useTranslation();
+    const more: string[] = Object.values(t('skills.more.data', { returnObjects: true })).map(value => String(value));
+    const data: string[] = Object.values(t('skills.data', { returnObjects: true })).map(value => String(value));
 
-  return (
-    <div className="my-16 px-3 font-sen text-white" id="skills">
-      <div className="flex items-center">
-        <p className="text-3xl font-bold text-white">{t('skills.title')}</p>
-        <div className="flex-grow border-t border-white ml-4"></div>
-      </div>
+    return (
+        <div className="my-16 px-3 font-sen text-white" id="skills">
+            <div className="flex items-center mb-8">
+                <p className="text-3xl font-bold text-white">{t('skills.title')}</p>
+                <div className="flex-grow border-t border-white ml-4"></div>
+            </div>
 
-      <div className="text-md my-8 flex font-medium md:text-xl custom:text-lg">
-        {data.map((skill: Skill) => (
-          <div key={skill.name}
-               className="flex flex-row items-center border-gray-500 py-1 text-slate-300">
-            <span
-              className="border border-lightText rounded-lg me-3 px-3 py-1"
-              key={skill.name}
-            >
-              {skill.name}
-            </span>
-          </div>
-        ))}
-      </div>
+            <div className="flex flex-wrap gap-3">
+                {data.map((skill: string) => (
+                    <div key={skill} className="inline-flex border border-lightText px-2 py-1 rounded-lg shadow-md transition-transform transform hover:scale-105">
+                        <span className="text-lg text-slate-300">{skill}</span>
+                    </div>
+                ))}
+            </div>
 
-      <p className="text-lg font-medium text-slate-300">
-        {t('skills.more.common')}{" "}
-        {more.map((skill: string, index: number) => (
-          <span key={skill} className="text-white">{skill}{index !== more.length - 1 && ", "}</span>
-        ))}
-      </p>
-    </div>
-  );
+            <p className="text-lg font-medium text-slate-300 mt-8">
+                {t('skills.more.common')}{" "}
+                {more.map((skill: string, index: number) => (
+                    <span key={skill} className="text-white">{skill}{index !== more.length - 1 && ", "}</span>
+                ))}
+            </p>
+        </div>
+    );
 };
 
 export default Skills;
